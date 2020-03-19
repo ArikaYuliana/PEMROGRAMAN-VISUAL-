5bfb450a655d7683926cd2894db647a726d78f0d
ArikaYuliana <ikay51221@gmail.com>
2020-03-19 14:51:16 +0800

    video 5

--- index.js
+++ index.js
@@ -65,6 +65,14 @@ const createWindowCreator = () => {
         console.log("here");
     });
 
+    ipcMain.on("appointment:request:today", event => {
+        console.log("here2");
+    });
+
+    ipcMain.on("appointment:done", (event, id) => {
+        console.log("here3");
+    });
+
 
 const menuTemplate= [{
     label: "File",
