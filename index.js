0dec2dca4ca28e64c07c14b49f588c48fe731072
ArikaYuliana <ikay51221@gmail.com>
2020-04-09 21:54:14 +0800

    video 6 & 7

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const electron = require("electron");
+const {
+    v4: uuidv4 
+} = require('uuid');
 
-const {app, BrowserWindow, Menu, ipcMain} = electron;
+const {
+    app,
+    BrowserWindow,
+    Menu,
+    ipcMain
+} = electron;
 
 let todayWindow;
 let createWindow;
 let listWindow;
 
+let allAppointment = [];
+
 app.on("ready", () => {
     todayWindow = new BrowserWindow({
-    webPreferences: {
-        nodeIntegration : true
-    },
-    title:"Aplikasi Doctor"
+        webPreferences: {
+            nodeIntegration: true
+        },
+        title: "Aplikasi Doctor"
     });
-    
+
     todayWindow.loadURL(`file://${__dirname}/today.html`);
     todayWindow.on("closed", () => {
 
@@ -28,22 +38,22 @@ app.on("ready", () => {
 
 
 const listWindowCreator = () => {
-listWindow = new BrowserWindow ({
-    webPreferences: {
-        nodeIntegration: true
-    },
-    width: 600,
-    height: 400,
-    title: "All Appointments"
-});
+    listWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "All Appointments"
+    });
 
-listWindow.setMenu(null);
-listWindow.loadURL(`file://${__dirname}/list.html`);
-listWindow.on ("closed", () => (listWindow = null ));
-}; 
+    listWindow.setMenu(null);
+    listWindow.loadURL(`file://${__dirname}/list.html`);
+    listWindow.on("closed", () => (listWindow = null));
+};
 
 const createWindowCreator = () => {
-    createWindow = new BrowserWindow ({
+    createWindow = new BrowserWindow({
         webPreferences: {
             nodeIntegration: true
         },
@@ -51,57 +61,67 @@ const createWindowCreator = () => {
         height: 400,
         title: "Create Appointments"
     });
-    
+
     createWindow.setMenu(null);
     createWindow.loadURL(`file://${__dirname}/create.html`);
-    createWindow.on ("closed", () => (createWindow = null ));
-    }; 
-    
-    ipcMain.on("appointment:create", (event, appointment) => {
-        console.log(appointment);
-    });
+    createWindow.on("closed", () => (createWindow = null));
+};
 
-    ipcMain.on("appointment:request:list", event => {
-        console.log("here");
-    });
+ipcMain.on("appointment:create", (event, appointment) => {
+    appointment["id"] = uuidv4();
+    appointment["done"] = 0;
+    allAppointment.push(appointment);
 
-    ipcMain.on("appointment:request:today", event => {
-        console.log("here2");
-    });
+    createWindow.close();
 
-    ipcMain.on("appointment:done", (event, id) => {
-        console.log("here3");
-    });
+    console.log(allAppointment);
+});
+
+ipcMain.on("appointment:request:list", event => {
+    listWindow.webContents.send('appointment:response:list', allAppointment);
+});
+
+ipcMain.on("appointment:request:today", event => {
+    console.log("here2");
+});
+
+ipcMain.on("appointment:done", (event, id) => {
+    console.log("here3")
+});
 
 
-const menuTemplate= [{
-    label: "File",
-submenu: [{
-        label: "New Appointment",
-        click() {
-            createWindowCreator();
+const menuTemplate = [{
+        label: "File",
+        submenu: [{
+                label: "New Appointment",
+                click() {
+                    createWindowCreator();
 
-        }
-     },
-     {
-        label: "All Appointment",
-        click() {
-           listWindowCreator();
-       } 
-     },
-     {
-         label: "Quit",
-         click() {
-            app.quit();
-    
-        } 
-     }
+                }
+            },
+            {
+                label: "All Appointment",
+                click() {
+                    listWindowCreator();
+                }
+            },
+            {
+                label: "Quit",
+                click() {
+                    app.quit();
+
+                }
+            }
+        ]
+    },
+
+    {
+        label: "View",
+        submenu: [{
+            role: "reload"
+        }, {
+            role: "toggledevtools"
+        }]
+
+    }
 ]
-},
-
-{
-    label: "View",
-    submenu: [{ role: "reload" }, {role: "toggledevtools"}]
-
-}
-] 
